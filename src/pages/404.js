a8104ae08ac30dc2fe850b642fe08a924f8faaa1
Mark <mark@larsen.org>
2019-09-14 01:32:16 -0600

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SectionTitle from "../components/sectiontitle";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const NotFoundPage = () => (
+    <Layout>
+        <SEO title="404: Not found" />
+        <section id="404" className="container" style={{ minHeight: "600px" }}>
+            <div className="section-title">
+                <SectionTitle title="404" />
+                <p class="text-primary">
+                    Oh darn, the page you are looking for doesn't exist :(
+                </p>
+            </div>
+        </section>
+    </Layout>
+);
+
+export default NotFoundPage;
